252fac5c91828e122cab3a51b933aee2a03a2d74
dev-eunjiLee <dev.eunji.lee@gmail.com>
2022-04-10 12:44:59 +0900

    [7.2] Coin Tracker

--- src/App.js
+++ src/App.js
@@ -1,36 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return; // * 함수가 동작하지 않는다.(=== return false)
-    }
-    setToDos((currentArray) => [...currentArray, toDo]);
-    setToDo("");
-  };
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  // * API를 최초 1회만 호출하기 위해 dependency state 배열을 비워줌
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        // console.log(json);
+        setCoins(() => json);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <div>
-      <div>Array length: {toDos.length}</div>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-      <div />
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {coins.map((coin) => (
+            <option key={coin.id}>
+              {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price}
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
 }
