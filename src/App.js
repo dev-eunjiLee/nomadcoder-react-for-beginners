53c2fecf05f58424ad3c97fa9410bec89d1c181b
dev-eunjiLee <dev.eunji.lee@gmail.com>
2022-04-13 23:10:20 +0900

    [7.4] Movie App part Two

--- src/App.js
+++ src/App.js
@@ -1,42 +1,5 @@
-import { useEffect, useState } from "react";
-
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        "https://yts.mx/api/v2/list_movies.json?minimun_rating=8.5&sort_by=year"
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-  useEffect(async () => {
-    await getMovies();
-  }, []);
-  return (
-    <div>
-      {loading ? (
-        <div>loading...</div>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} />
-              <h2>{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.genres.map((g) => {
-                  return <li key={g}>{g}</li>;
-                })}
-              </ul>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  return null;
 }
 
 export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import Movie from "../components/Movie";
+
+function Home() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        "https://yts.mx/api/v2/list_movies.json?minimun_rating=8.5&sort_by=year"
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    (async () => {
+      await getMovies();
+    })();
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <div>loading...</div>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              genres={movie.genres}
+              summary={movie.summary}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Home;
