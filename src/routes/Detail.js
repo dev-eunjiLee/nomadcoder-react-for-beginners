26c126c60051cc770f4bd42eb47da8640c54ed0f
dev-eunjiLee <dev.eunji.lee@gmail.com>
2022-05-01 17:06:47 +0900

    [7.6] Parameters

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,22 @@
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+
 function Detail() {
+  // router에서 내가 지정한 변수명을 그대로 들고온다
+  const { id } = useParams();
+
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+
+    console.log(json);
+  };
+
+  useEffect(async () => {
+    const json = getMovie();
+  }, []);
+
   return <h1>Detail</h1>;
 }
 
