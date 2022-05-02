a729011cc6f705295a36ead13f47ed4a7f87b300
dev-eunjiLee <dev.eunji.lee@gmail.com>
2022-05-02 23:34:54 +0900

    [7.6] Code Challenge

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,23 +1,52 @@
 import { useParams } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Detail() {
   // router에서 내가 지정한 변수명을 그대로 들고온다
   const { id } = useParams();
+
+  const [loading, setLoading] = useState(true);
+  const [movieDetail, setMovieDetail] = useState();
 
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
 
-    console.log(json);
+    console.log(json.data.movie);
+
+    setMovieDetail(json.data.movie);
+    setLoading(false);
   };
 
   useEffect(async () => {
     const json = getMovie();
   }, []);
 
-  return <h1>Detail</h1>;
+  return loading ? (
+    <div>loading...</div>
+  ) : (
+    <div>
+      <h1>{movieDetail.title}</h1>
+      <img src={movieDetail.medium_cover_image} />
+      <div>
+        <h3>genres</h3>
+        <ul>
+          {movieDetail.genres.map((perGenre) => {
+            return <li>{perGenre}</li>;
+          })}
+        </ul>
+      </div>
+      <div>
+        <h3>description</h3>
+        {movieDetail.description_intro !== "" ? (
+          <p> movieDetail.description_intro</p>
+        ) : (
+          <p>여튼 재밌는 영화야</p>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Detail;
